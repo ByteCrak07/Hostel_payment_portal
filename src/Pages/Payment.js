2e011311d0b0ce471aba5d631d7bff721ad378f9
Rishika <rishikarocky14@gmail.com>
2020-08-16 23:06:06 +0530

    toggler pending

--- src/Pages/Payment.js
+++ src/Pages/Payment.js
@@ -43,69 +43,68 @@ class Payment extends Component {
   render() {
     return (
       <div className="payment">
-        <div className="content ">
-          <Jumbotron>
-            <div className="container">
-              <div className="row">
-                <div className="col headers d-flex">
-                  <span className="fa fa-user fa-2x"></span>
-                  <h3> &nbsp;Firstname Lastname &nbsp;({this.state.reg_no})</h3>
-                </div>
-              </div>
-            </div>
-          </Jumbotron>
+        <Jumbotron>
           <div className="container">
-            <div className="row " style={{ marginTop: "5vh" }}>
-              <div className="col-md-7 headers">
-                <h2>
-                  Amount <span className="fa fa-caret-right f-2x"></span> Rs.{this.state.fees}/-
-                </h2>
-              </div>
-              <div className="col-10 offset-1 col-md-4 offset-md-1">
-                <Button className="pay btn-block btn-lg mt-2">Pay</Button>
+            <div className="row">
+              <div className="col headers d-flex">
+                <span className="fa fa-user fa-2x profile-emb"></span>
+                <h3> &nbsp;Firstname Lastname &nbsp;({this.state.reg_no})</h3>
               </div>
             </div>
-            <hr />
-            <div className="row history">
-              <Nav tabs>
-                <NavItem>
-                  <NavLink
-                    className={{ active: this.state.activeTab === "1" }}
-                    id="recent"
-                    onClick={() => {
-                      this.toggle("1");
-                    }}
-                  >
-                    Recent payments
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={{ active: this.state.activeTab === "2" }}
-                    id="pending"
-                    onClick={() => {
-                      this.toggle("2");
-                    }}
-                  >
-                    Pending payments
-                  </NavLink>
-                </NavItem>
-              </Nav>
-              <TabContent activeTab={this.state.activeTab}>
-                <TabPane tabId="1">
-                  <Row style={{ marginTop: "7vh" }}>
-                    <Col sm="12">
-                      <h4>Tab 1 Contents</h4>
-                    </Col>
-                  </Row>
-                </TabPane>
-                <TabPane tabId="2">
-                  <Row>
-                    <Col sm="6">Hey</Col>
-                  </Row>
-                </TabPane>
-              </TabContent>
+          </div>
+        </Jumbotron>
+        <div className="container">
+          <div className="row " style={{ marginTop: "5vh" }}>
+            <div className="col-md-7 headers">
+              <h2>
+                Amount <span className="fa fa-caret-right f-2x"></span> Rs.
+                {this.state.fees}/-
+              </h2>
             </div>
+            <div className="col-10 offset-1 col-md-4 offset-md-1">
+              <Button className="pay btn-block btn-lg mt-2">Pay</Button>
+            </div>
+          </div>
+          <hr />
+          <div className="row history">
+            <Nav tabs>
+              <NavItem>
+                <NavLink
+                  className={{ active: this.state.activeTab === "1" }}
+                  id="recent"
+                  onClick={() => {
+                    this.toggle("1");
+                  }}
+                >
+                  Recent payments
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={{ active: this.state.activeTab === "2" }}
+                  id="pending"
+                  onClick={() => {
+                    this.toggle("2");
+                  }}
+                >
+                  Pending payments
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <TabContent activeTab={this.state.activeTab}>
+              <TabPane tabId="1">
+                <Row style={{ marginTop: "7vh" }}>
+                  <Col sm="12">
+                    <h4>Tab 1 Contents</h4>
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tabId="2">
+                <Row>
+                  <Col sm="6">Hey</Col>
+                </Row>
+              </TabPane>
+            </TabContent>
           </div>
         </div>
       </div>
--- src/Pages/Sidebar.js
+++ src/Pages/Sidebar.js
@@ -1,56 +1,79 @@
-import React from 'react';
+import React from "react";
 import { useLocation } from "react-router-dom";
-import '../Styles/Sidebar.css';
+import "../Styles/Sidebar.css";
 import gectemblem from "../Assets/Images/gectemblem.jpg";
 
 function Sidebar(props) {
-    let sideboard_class = "sideboard";
-    if(props.width < 768 && props.collapse) {
-        sideboard_class = "sideboard sideboard-collapsed"
-    } else if(props.width < 768) {
-        sideboard_class = "sideboard sideboard-float"
-    }
-    
-    let path = useLocation().pathname;
-    let [option_class1, option_class2, option_class3] = ["option", "option", "option"];
-    if(path === "/dashboard/profile"){
-        option_class1 += " active-menu"
-    }
-    if(path === "/dashboard/payment"){
-        option_class2 += " active-menu"
-    }
-    if(path === "/dashboard/notifications"){
-        option_class3 += " active-menu"
-    }
+  let sideboard_class = "sideboard";
+  if (props.width < 768 && props.collapse) {
+    sideboard_class = "sideboard sideboard-collapsed";
+  } else if (props.width < 768) {
+    sideboard_class = "sideboard sideboard-float";
+  }
 
-    return (
-        <div className={sideboard_class}>
-            <div className="container">
-                <div className="head" onClick={() => props.collapseToggle()}>
-                    <div>
-                        <img src={gectemblem} className="emblem" alt="emblem" />
-                    </div>
-                    <div className="text">&nbsp;GECT&nbsp;PORTAL</div>
-                </div>
-                <hr />
-                <div className={option_class1} onClick={() => props.routeToggle("profile")}>
-                    <div className="col">
-                        <span className="fa fa-user fa-2x"></span><span className="text">&nbsp;USER PROFILE</span>
-                    </div>
-                </div>  
-                <div className={option_class2} onClick={() => props.routeToggle("payment")}>
-                    <div className="col">
-                        <span className="fa fa-credit-card-alt fa-2x"></span><span className="text">&nbsp;PAY MY FEE</span>
-                    </div>
-                </div>  
-                <div className={option_class3} onClick={() => props.routeToggle("notifications")}>
-                    <div className="col">
-                        <span className="fa fa-bell-o fa-2x"></span><span className="text">&nbsp;NOTIFICATIONS</span>
-                    </div>
-                </div>
+  let path = useLocation().pathname;
+  let [option_class1, option_class2, option_class3] = [
+    "option",
+    "option",
+    "option",
+  ];
+  if (path === "/dashboard/profile") {
+    option_class1 += " active-menu";
+  }
+  if (path === "/dashboard/payment") {
+    option_class2 += " active-menu";
+  }
+  if (path === "/dashboard/notifications") {
+    option_class3 += " active-menu";
+  }
+
+  return (
+    <React.Fragment>
+      <div className="toggler ">
+        <img src={gectemblem} className="emblem" alt="navbar" />
+      </div>
+      <div className={sideboard_class}>
+        <div className="container">
+          <div className="head" onClick={() => props.collapseToggle()}>
+            <div>
+              <img src={gectemblem} className="emblem" alt="emblem" />
             </div>
+            <div className="text ">&nbsp;GECT&nbsp;PAY</div>
+          </div>
+          <hr />
+          <div className="options">
+            <div
+              className={option_class1}
+              onClick={() => props.routeToggle("profile")}
+            >
+              <div className="col">
+                <span className="fa fa-user fa-2x "></span>
+                <span className="text">&nbsp;USER PROFILE</span>
+              </div>
+            </div>
+            <div
+              className={option_class2}
+              onClick={() => props.routeToggle("payment")}
+            >
+              <div className="col">
+                <span className="fa fa-credit-card-alt fa-2x"></span>
+                <span className="text">&nbsp;PAY MY FEE</span>
+              </div>
+            </div>
+            <div
+              className={option_class3}
+              onClick={() => props.routeToggle("notifications")}
+            >
+              <div className="col">
+                <span className="fa fa-bell-o fa-2x"></span>
+                <span className="text">&nbsp;NOTIFICATIONS</span>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Sidebar;
